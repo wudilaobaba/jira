59b81087f308988e0e56e652ffbf6d6b52d94fdc
whj <1273803935@qq.com>
2022-01-15 21:37:53 +0800

    fix: d

--- src/screens/peoject-list/index.jsx
+++ src/screens/peoject-list/index.jsx
@@ -1,17 +1,16 @@
 import { SearchPanel } from "./search-panel";
-import { List } from "./list"
-import { useEffect, useState } from 'react'
-import { cleanObject, useMount } from "../../utils"
-import qs from "qs"
-
-const apiUrl = process.env.REACT_APP_API_URL
+import { List } from "./list";
+import { useEffect, useState } from "react";
+import { cleanObject, useMount } from "../../utils";
+import qs from "qs";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
   const [param, setParam] = useState({ name: "", personId: "" });
   const [list, setList] = useState([]);
   useEffect(() => {
-    fetch(`${apiUrl}/users`).then(async response => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
@@ -19,19 +18,20 @@ export const ProjectListScreen = () => {
   }, []);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
-      if (response.ok) {
-        const result = await response.json();
-        setList(result);
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          const result = await response.json();
+          setList(result);
+        }
       }
-    });
+    );
   }, [param]);
-
 
   return (
     <>
-      <SearchPanel users={users} param={param} setParam={setParam}/>
-      <List list={list} users={users}/>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List list={list} users={users} />
     </>
   );
 };
